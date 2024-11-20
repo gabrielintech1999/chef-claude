b8e8d93855cd544e91ffb034f60fd16ce50450cb
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-11-20 23:22:16 +0000

    basic setup and deploy

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-black text-white h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+    </div>
+  );
+}
